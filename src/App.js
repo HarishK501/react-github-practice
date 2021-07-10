d26d832f850e5b079a13cb86f24646a4aa2f34a7
HarishK501 <harishcse18501@gmail.com>
2021-07-11 00:15:22 +0530

    modified title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Harish K - React App & GitHub pages
+          Harish K - React App
         </p>
         <a
           className="App-link"
